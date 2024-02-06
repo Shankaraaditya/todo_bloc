fb8ba6f6aef00bf0f78bc003cd5b6100f804e6a1
Shankaraaditya <aditya.20b0121083@abes.ac.in>
2024-02-07 03:46:57 +0530

    todo Model created
    todo model created confirm

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -1,8 +1,7 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
-part 'todo_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_bloc/models/todo_model.dart';
 
-class TodoCubit extends Cubit<TodoState> {
-  TodoCubit() : super(TodoInitial());
+class TodoCubit extends Cubit<List<Todo>> {
+  TodoCubit() : super([]);
 }
